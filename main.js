8cc2b4586e19c36e44534e9902ffbeda0290c24b
Luis <s.ros.lui@gmail.com>
2020-11-16 06:31:37 +0100

    add

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 console.log("ok funziona!", Vue);
 
 const app = new Vue({
+    // el: '#app',
+    // data: {
+    //     nome: '',
+    //     cognome: '',
+    //     testo:''
+    // }
+
+
     el: '#app',
     data: {
-        nome: '',
-        cognome: '',
-        testo:''
+        message: 'Benvenuto in ...',
+        logo: './logo.jpeg',
+        title: 'Mia App'
     }
 });
